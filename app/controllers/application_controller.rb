e9ee351295e33c6e187c68695e5b3ac4f4b9c5cf
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-05 12:46:05 -0500

    added temps to chart, delete persists but not yet disappearing before reload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,18 @@ class ApplicationController < Sinatra::Base
 
   get "/stars" do 
     stars = Star.all;
-    stars.to_json({only: [:id, :name, :distance, :apparent_magnitude, :confirmed_planets]});
+    stars.to_json({only: [:id, :name, :distance, :apparent_magnitude, :temperature, :confirmed_planets]});
   end
 
   get "/users" do 
     users = User.all;
     users.to_json({only: :username})
+  end
+
+  delete "/stars/:id" do
+    star = Star.find(params[:id])
+    star.destroy
+    star.to_json
   end
 
 end
